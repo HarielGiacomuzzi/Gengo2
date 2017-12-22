1eece90cecceb668e75dec2b9ad3e07e9bb7c1c3
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2017-12-22 15:55:06 -0200

    initial commit

---
+++ Gengo2/Gengo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gengo2
+//
+//  Created by Hariel Giacomuzzi on 22/12/17.
+//  Copyright © 2017 Giacomuzzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
